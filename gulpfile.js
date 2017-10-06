8eb6795be6782560fbc0bac976bf65f2010b0ec4
esleping <esleping1002@gmail.com>
2017-10-06 14:10:37 -0500

    produccion

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+useref = require('gulp-useref'),
+gulpif = require('gulp-if'),
+uglify = require('gulp-uglify'),
+minifyCss = require('gulp-clean-css');
 var server = require('gulp-server-livereload');
  
 gulp.task('server', function() {
@@ -8,4 +12,11 @@ gulp.task('server', function() {
       directoryListing: false,
       open: true
     }));
+});
+gulp.task('Build', function () {
+  return gulp.src(['*.html','pa*/*.html','im*/*.jpg'])
+      .pipe(useref())
+      .pipe(gulpif('*.js', uglify()))
+      .pipe(gulpif('*.css', minifyCss()))
+      .pipe(gulp.dest('dist'));
 });
